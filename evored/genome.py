1ff6abdd1c374a388fd15e3ccfe85b8128896fe1
Will Knez <wbknez.dev@gmail.com>
2017-11-30 13:43:22 -0700

    Add object that represents a single Redcode instruction in a syntax tree.

--- evored/genome.py
+++ evored/genome.py
@@ -19,3 +19,27 @@ class Fitnessable(metaclass=ABCMeta):
 
     def __str__(self):
         return "(f:%i)" % self.fitness
+
+
+class Chromosome(Fitnessable):
+    """
+    Represents a single Redcode instruction in a probabilistic syntax tree.
+    """
+
+    def __init__(self, ins, fitness=0):
+        super().__init__(fitness)
+        self.ins = ins
+
+    def __eq__(self, other):
+        if isinstance(other, Chromosome):
+            return self.fitness == other.fitness and self.ins == other.ins
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.fitness, self.ins))
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __str__(self):
+        return "(%s,%i)" % (self.ins, self.fitness)
