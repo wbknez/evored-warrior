26917e36b2b22883104127f440acedd8e32ed863
Will Knez <wbknez.dev@gmail.com>
2017-11-29 20:18:05 -0700

    Add application entry point.

---
+++ evored/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+The main driver for the Evolutionary Redcode Warrior project, a simple
+example of creating Redcode warriors using probabilistic syntax trees.
+"""
+import sys
+
+
+def main():
+    """
+    The application entry point.
+
+    :return: An exit code.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
