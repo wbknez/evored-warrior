9c66bf07fe03ba6e4cf96a5a79c7fdd6b0cb5ee7
Will Knez <wbknez.dev@gmail.com>
2017-12-02 17:18:33 -0700

    Add randomized implementation of GenePool and add extraction method to generate a list of genes.

--- evored/gene_pool.py
+++ evored/gene_pool.py
@@ -3,6 +3,9 @@ Contains all classes and functions pertaining to the random generation of
 Redcode instructions.
 """
 from abc import ABCMeta, abstractmethod
+from random import choice, randrange
+
+from evored.lang import OpCode, Modifier, AddressMode, Instruction, Argument
 
 
 class GenePool(metaclass=ABCMeta):
@@ -13,6 +16,15 @@ class GenePool(metaclass=ABCMeta):
     that is not coupled to a fitness score.
     """
 
+    def extract(self, count):
+        """
+        Creates a list of new genes with the specified size.
+
+        :param count: The number of genes to extract from the pool.
+        :return: A list of new genes.
+        """
+        return [self.next_gene for _ in range(0, count)]
+
     @abstractmethod
     def next_gene(self):
         """
@@ -21,3 +33,32 @@ class GenePool(metaclass=ABCMeta):
         :return: A new random gene.
         """
         pass
+
+
+class RandomGenePool(GenePool):
+    """
+    Represents an implementation of GenePool that randomly chooses from a
+    collection of operation codes, modifiers, addressing modes, and argument
+    ranges to construct a single gene, or Redcode instruction.
+
+    Attributes:
+        addr_modes (list): The list of available addressing modes.
+        arg_range (tuple): The range of values an argument may have.
+        modifiers (list): The list of available instruction modifiers.
+        opcodes (list): The list of available operation codes.
+    """
+
+    def __init__(self, opcodes=list(OpCode), modifiers=list(Modifier),
+                 addr_modes=list(AddressMode), arg_range=(0, 8000)):
+        super().__init__()
+        self.opcodes = opcodes
+        self.modifiers = modifiers
+        self.addr_modes = addr_modes
+        self.arg_range = arg_range
+
+    def next_gene(self):
+        return Instruction(choice(self.opcodes), choice(self.modifiers),
+                           Argument(choice(self.addr_modes),
+                                    randrange(self.arg_range)),
+                           Argument(choice(self.addr_modes),
+                                    randrange(self.arg_range)))
