e307efdaa74049634a1db55507570b41b873ed7a
Will Knez <wbknez.dev@gmail.com>
2017-12-09 15:04:08 -0700

    Update comments.

--- evored/mutation.py
+++ evored/mutation.py
@@ -6,18 +6,18 @@ from abc import ABCMeta, abstractmethod
 
 class Mutator(metaclass=ABCMeta):
     """
-    Represents a mechanism for altering an arbitrary object (in this case,
-    a genome or an instruction argument) in some way, with the stipulation
-    that such changes are made in an attempt to improve its overall fitness.
+    Represents a mechanism for altering a genome in some way, with the
+    stipulation that such changes are made in an attempt to improve its
+    overall fitness.
     """
 
     @abstractmethod
-    def mutate(self, obj, params):
+    def mutate(self, genome, params):
         """
-        Mutates the specified object in some way, influenced by the specified
+        Mutates the specified genome in some way, influenced by the specified
         user-selected parameters.
 
-        :param obj: The object to mutate.
+        :param genome: The object to mutate.
         :param params: A dictionary of parameters.
         """
         pass
