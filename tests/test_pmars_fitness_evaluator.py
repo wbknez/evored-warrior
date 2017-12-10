25f21af1c58441efd9d3aa8a42a7619093351f5f
Will Knez <wbknez.dev@gmail.com>
2017-12-09 18:02:46 -0700

    Add skeleton test suite for PmarsFitnessEvaluator.

---
+++ tests/test_pmars_fitness_evaluator.py
@@ -0,0 +1,13 @@
+"""
+Contains unit tests for verifying correctness of PMARS related fitness
+evaluation algorithms.
+"""
+from unittest import TestCase
+
+
+class PmarsFitnessEvaluatorTest(TestCase):
+    """
+    Test suite for PmarsFitnessEvaluator.
+    """
+
+    pass
