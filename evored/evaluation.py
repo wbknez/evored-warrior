e54dfc91792a29ab08e8d31f1772876eb04a4af2
Will Knez <wbknez.dev@gmail.com>
2017-12-10 13:50:59 -0700

    Add function(s) to parse processes output and update comments.

--- evored/evaluation.py
+++ evored/evaluation.py
@@ -47,31 +47,10 @@ class FitnessEvaluator(metaclass=ABCMeta):
         pass
 
 
-def convert_to_temp_files(warriors):
-    return []
-
-
-def parse_output(stdout):
-    """
-
-    :param stdout:
-    :return:
-    """
-    scores = []
-    for line in stdout:
-        try:
-            index = line.index("scores") + PmarsFitnessEvaluator.SCORE_OFFSET
-            scores.append(int(line[index:]))
-        except ValueError:
-            pass
-    return scores
-
-
 class PmarsFitnessEvaluator(FitnessEvaluator):
     """
     Represents an implementation of FitnessEvaluator that uses an external
-    program, PMARS, to evaluate warriors for fitness against to one or more
-    benchmarks.
+    program, PMARS, to evaluate warriors for fitness.
     """
 
     DEFAULT_ASM_OUTPUT = False
@@ -82,6 +61,14 @@ class PmarsFitnessEvaluator(FitnessEvaluator):
     DEFAULT_CORE_SIZE = 8000
     """
     The default core memory size.
+    """
+
+    DEFAULT_PMARS_PATH = "bin/pmars"
+    """
+    The default location of the PMARS executable.
+    
+    Please note that this is a relative path.  PMARS must be compiled with 
+    the -DSERVER switch in order to disable the graphical display entirely.
     """
 
     DEFAULT_ROUNDS = 50
@@ -104,42 +91,66 @@ class PmarsFitnessEvaluator(FitnessEvaluator):
 
     def build_command(self, params):
         """
+        Creates and caches the base command used to invocate PMARS with the
+        specified parameters, if any.
 
-        :param params:
+        :param params: A dictionary of parameters.
         """
-        exe = params.get("pmars_path", "/usr/bin/pmars")
+        exe = params.get("pmars.path", PmarsFitnessEvaluator.DEFAULT_PMARS_PATH)
 
         # PMARS options.
         asm = params.get("pmars.asm",
                          PmarsFitnessEvaluator.DEFAULT_ASM_OUTPUT)
-        coresize = params.get("pmars.core_size",
-                          PmarsFitnessEvaluator.DEFAULT_CORE_SIZE)
+        core_size = params.get("pmars.core_size",
+                               PmarsFitnessEvaluator.DEFAULT_CORE_SIZE)
         rounds = params.get("pmars.rounds",
                             PmarsFitnessEvaluator.DEFAULT_ROUNDS)
         verbose = params.get("pmars.verbose",
                              PmarsFitnessEvaluator.DEFAULT_VERBOSITY)
 
-        self.cmd = [exec, "-r", str(rounds), "-s", str(coresize)]
+        self.cmd = [exe, "-r", str(rounds), "-s", str(core_size)]
 
-        if asm:
-            self.cmd.extend("-b")
+        if not asm:
+            self.cmd.append("-b")
 
         if verbose:
-            self.cmd.extend("-V")
+            self.cmd.append("-V")
 
     def evaluate(self, warriors, params):
-        if not warriors:
-            raise EvaluationException("There are no warriors to evaluate.")
-
-        if not params["benchmarks"]:
-            raise EvaluationException("There are not benchmarks to use for "
+        if len(warriors) <= 1:
+            raise EvaluationException("There are not enough warriors for "
                                       "evaluation.")
 
         if not self.cmd:
             self.build_command(params)
 
-        sources = convert_to_temp_files(warriors)
-        full_cmd = self.cmd + sources + params["benchmarks"]
+        full_cmd = self.cmd + self.write_to_temp(warriors, params)
         pmars_pid = subprocess.Popen(full_cmd, shell=True,
                                      stdout=subprocess.PIPE)
-        return parse_output(pmars_pid.stdout)
+        return self.parse_output(pmars_pid.stdout)
+
+    def parse_output(self, stream):
+        """
+        Extracts the PMARS-generated fitness scores from the specified output
+        stream.
+
+        :param stream: The output stream to parse.
+        :return: The generated fitness scores.
+        """
+        scores = []
+        for line in stream:
+            try:
+                index = line.index("scores") + PmarsFitnessEvaluator.SCORE_OFFSET
+                scores.append(int(line[index:]))
+            except ValueError:
+                pass
+        return scores
+
+    def write_to_temp(self, warriors, params):
+        """
+
+        :param warriors:
+        :param params:
+        :return:
+        """
+        return []
