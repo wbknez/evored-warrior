cb83f2e3fbf76bcf4e75601cab59e4952e2dc271
Will Knez <wbknez.dev@gmail.com>
2017-12-10 17:45:48 -0700

    Add object that represents a single warrior.

--- evored/genome.py
+++ evored/genome.py
@@ -35,11 +35,6 @@ class Chromosome(Fitnessable):
         self.ins = ins
 
     def __copy__(self):
-        """
-        Creates a copy of this chromosome as a new object.
-
-        :return: A new chromosome.
-        """
         return Chromosome(self.ins, self.fitness)
 
     def __eq__(self, other):
@@ -55,6 +50,40 @@ class Chromosome(Fitnessable):
 
     def __str__(self):
         return "(%s,%i)" % (self.ins, self.fitness)
+
+
+class Warrior(Fitnessable):
+    """
+    Represents a single, stochastic realization of a genome tree.
+
+    This warrior object itself represents a collection of Redcode assembly
+    instructions that have been created by a probabilistic depth-first
+    traversal of a binary tree of chromosomes to a leaf.  Each warrior has a
+    fitness score that is determined by its performance against other
+    warriors using a Core Wars simulator, in this case the PMARS program.
+    """
+
+    def __init__(self, ins_list, fitness=0):
+        super().__init__(fitness)
+        self.ins_list = ins_list
+
+    def __copy__(self):
+        return Warrior(self.ins_list, self.fitness)
+
+    def __eq__(self, other):
+        if isinstance(other, Warrior):
+            return self.fitness == other.fitness and \
+                   self.ins_list == other.ins_list
+        return NotImplemented
+
+    def __hash__(self):
+        return hash((self.fitness, self.ins_list))
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __str__(self):
+        return "\n".join(map(str, self.ins_list))
 
 
 class Genome(Fitnessable, Tree):
