4338b17a2ab7d83dd739c6105aa194bef20af7b3
Will Knez <wbknez.dev@gmail.com>
2017-12-22 15:47:10 -0700

    Add comparison operations for Fitness to allow the mutation, selection, and crossover algorithms to be generic.

--- evored/genome.py
+++ evored/genome.py
@@ -18,6 +18,34 @@ class Fitnessable(metaclass=ABCMeta):
     def __init__(self, fitness=0):
         self.fitness = fitness
 
+    def __eq__(self, other):
+        if isinstance(other, Fitnessable):
+            return self.fitness == other.fitness
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Fitnessable):
+            return self.fitness >= other.fitness
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Fitnessable):
+            return self.fitness > other.fitness
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Fitnessable):
+            return self.fitness <= other.fitness
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Fitnessable):
+            return self.fitness < other.fitness
+        return NotImplemented
+
+    def __ne__(self, other):
+        return not self == other
+
     def __repr__(self):
         return repr(self.fitness)
 
@@ -92,6 +120,6 @@ class Genome(Fitnessable, Tree):
     singular Redcode instructions.
     """
 
-    def __init__(self):
+    def __init__(self, chromosomes=None):
         Fitnessable.__init__(self)
-        Tree.__init__(self)
+        Tree.__init__(self, chromosomes)
