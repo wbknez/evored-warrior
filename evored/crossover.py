c2d3f769a0ed062373fdbba9c9ff30d6a905d287
Will Knez <wbknez.dev@gmail.com>
2018-01-21 19:26:32 -0700

    Update crossover algorithm to be compatible with parallel processing.

--- evored/crossover.py
+++ evored/crossover.py
@@ -1,10 +1,16 @@
 """
 Contains all classes and functions pertaining to methods of genome crossover.
 """
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
+from functools import partial
+from math import floor
+from random import random, shuffle
+
+from evored.evolution import EvolvingAlgorithm
+from evored.utils import flatten
 
 
-class Crossover(metaclass=ABCMeta):
+class Crossover(EvolvingAlgorithm):
     """
     Represents a mechanism for swapping portions of two genome's genetic
     information with each other.
@@ -22,3 +28,19 @@ class Crossover(metaclass=ABCMeta):
         :return: A list containing the genomes after crossover.
         """
         pass
+
+    def evolve(self, genomes, pool, params):
+        shuffle(genomes)
+        pair_a, pair_b = self.extract_crossing_pairs(genomes, params)
+
+        binding = partial(self.cross, params=params)
+        return genomes + flatten(pool.map(binding, pair_a, pair_b))
+
+    def extract_crossing_pairs(self, genomes, params):
+        crossing_pairs = []
+        for i in range(0, len(genomes) - 1, 2):
+            if random() < params["crossover.rate"]:
+                crossing_pairs.append(genomes.pop(i))
+                crossing_pairs.append(genomes.pop(i + 1))
+        half_width = floor(len(crossing_pairs) / 2)
+        return [crossing_pairs[:half_width], crossing_pairs[half_width:]]
