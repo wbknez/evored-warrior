d367a9b33444b275416fe564860deff697c866a2
Will Knez <wbknez.dev@gmail.com>
2018-01-23 07:18:42 -0700

    Add method to generate a sized list of random genomes.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,8 +7,26 @@ from random import randint
 from evored.genome import Genome
 
 
-def random_genome_list(fitness_scores, max_chromosome_num=10,
-                       max_chromosome_value=100):
+def create_genomes(num_genomes, max_fitness=100, max_chromosome_num=10,
+                   max_chromosome_value=100):
+    """
+    Create a list of genomes of random structure and fitness using the
+    specified parameters.
+
+    :param num_genomes: The number of genomes to create.
+    :param max_fitness: The maximum fitness score a genome may have.
+    :param max_chromosome_num: The maximum number of chromosomes a genome may
+    have.
+    :param max_chromosome_value: The maximum value a single chromosome may have.
+    :return: A list of randomly generated genomes.
+    """
+    fitness_scores = [randint(0, max_fitness) for _ in range(num_genomes)]
+    return create_genomes_from_fitness(fitness_scores, max_chromosome_num,
+                                       max_chromosome_value)
+
+
+def create_genomes_from_fitness(fitness_scores, max_chromosome_num=10,
+                                max_chromosome_value=100):
     """
     Creates a list of genomes whose structures are randomly generated but
     whose fitness scores are not, making them suitable for testing algorithms
