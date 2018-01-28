512a4d05f3a32ea23cbdbb4a1873151afc87894f
Will Knez <wbknez.dev@gmail.com>
2018-01-28 09:17:54 -0700

    Remove file caching mechanisms because they are unnecessary.

--- evored/utils.py
+++ evored/utils.py
@@ -7,7 +7,6 @@ import os
 import tempfile
 
 import shutil
-from abc import ABCMeta, abstractmethod
 
 
 def flatten(llist):
@@ -55,59 +54,3 @@ def tempdir():
 
     with cd(dir_path, cleanup):
         yield dir_path
-
-
-class FilenameGenerator(metaclass=ABCMeta):
-    """
-    Represents a mechanism to reate new, unique names for use as temporary
-    file descriptors.
-    """
-
-    @abstractmethod
-    def next_file(self):
-        """
-        Creates and returns a new file name for use.
-
-        :return: A new file name.
-        """
-        pass
-
-
-class SequentialFilenameGenerator(FilenameGenerator):
-    """
-    Represents an implementation of FilenameGenerator that produces file
-    names based on simple, incremental scheme.
-    """
-
-    def __init__(self, prefix="__temp__file__", ext="RED"):
-        self.ext = ext
-        self.idx = 0
-        self.prefix = prefix
-
-    def next_file(self):
-        self.idx += 1
-        return self.prefix + str(self.idx) + "." + self.ext
-
-
-class FileCache:
-    """
-    Represents a mechanism for creating, caching, and reusing temporary file
-    names.
-    """
-
-    def __init__(self, generator):
-        self.cache = []
-        self.generator = generator
-
-    def __iter__(self):
-        for cached in self.cache:
-            yield cached
-
-    def borrow(self):
-        return self.cache.pop(0) if self.cache else self.generator.next_file()
-
-    def reuse(self, file):
-        self.cache.append(file)
-
-    def reuse_all(self, files):
-        self.cache.extend(files)
