6382a9ace62e411e58ca963b01db3623c4e34d7a
Will Knez <wbknez.dev@gmail.com>
2018-01-28 06:14:32 -0700

    Update Fitnessable to reside in the fitness package.

--- evored/genome.py
+++ evored/genome.py
@@ -2,55 +2,8 @@
 Contains all the classes and functions that comprise the conceptual model for
 this project.
 """
-from abc import ABCMeta
-
+from evored.fitness import Fitnessable
 from evored.tree import Tree, _copy_tree
-
-
-class Fitnessable(metaclass=ABCMeta):
-    """
-    Represents an object that contains a concept of fitness.
-
-    Attributes:
-        fitness (int): A fitness score.
-    """
-
-    def __init__(self, fitness=0):
-        self.fitness = fitness
-
-    def __eq__(self, other):
-        if isinstance(other, Fitnessable):
-            return self.fitness == other.fitness
-        return NotImplemented
-
-    def __ge__(self, other):
-        if isinstance(other, Fitnessable):
-            return self.fitness >= other.fitness
-        return NotImplemented
-
-    def __gt__(self, other):
-        if isinstance(other, Fitnessable):
-            return self.fitness > other.fitness
-        return NotImplemented
-
-    def __le__(self, other):
-        if isinstance(other, Fitnessable):
-            return self.fitness <= other.fitness
-        return NotImplemented
-
-    def __lt__(self, other):
-        if isinstance(other, Fitnessable):
-            return self.fitness < other.fitness
-        return NotImplemented
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __repr__(self):
-        return repr(self.fitness)
-
-    def __str__(self):
-        return "(f:%i)" % self.fitness
 
 
 class Chromosome(Fitnessable):
